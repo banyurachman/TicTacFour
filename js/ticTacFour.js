c52f8bad989972370b3245696f5a1e69b0656582
banyurachman <banyurachman95@gmail.com>
2018-07-23 20:54:08 +0700

    Complete the rule algorithm.

--- js/ticTacFour.js
+++ js/ticTacFour.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   for(var i = 1 ; i <= 4 ; i++){
     val[i] = [];
     for(var j = 1 ; j <= 4 ; j++){
-        val[i][j] = 0;
+        val[i][j] = 1;
     }
   }
 
@@ -25,7 +25,7 @@ $(document).ready(function(){
 
   //final_check
   function final_check(total){
-    if(total == 4 || total == 36){
+    if(total == 40 || total == 400){
       return 1;
     }else{
       return 0;
@@ -72,6 +72,75 @@ $(document).ready(function(){
     return final_check(total);
   }
 
+  //Check for block top left
+  function check_top_left(row, col){
+    return final_check(val[row][col] + val[row+1][col] + val[row][col+1] + val[row+1][col+1]);
+  }
+
+  //Check for block top right
+  function check_top_right(row, col){
+    return final_check(val[row][col] + val[row][col-1] + val[row+1][col] + val[row+1][col-1]);
+  }
+
+  //Check for block bottom left
+  function check_bottom_left(row, col){
+    return final_check(val[row][col] + val[row-1][col] + val[row][col+1] + val[row-1][col+1]);
+  }
+
+  //Check for block bottom right
+  function check_bottom_right(row, col){
+    return final_check(val[row][col] + val[row-1][col] + val[row][col-1] + val[row-1][col-1]);
+  }
+
+  //Check for block check_block_recursive
+  function check_block_recursive(row, col, dataRow, dataCol){
+
+    if(dataRow == 'top'){
+      if(dataCol == 'left'){
+        return(check_top_left(row, col));
+      }else{
+        return(check_top_right(row, col));
+      }
+    }else{
+      if(dataCol == 'left'){
+        return(check_bottom_left(row, col));
+      }else{
+        return(check_bottom_right(row, col));
+      }
+    }
+  }
+
+  //Check for block
+  function check_block(row, col){
+
+    var row_validator = [];
+    var col_validator = [];
+    if(row != 1){
+      row_validator.push('bottom');
+    }
+    if(row != 4){
+      row_validator.push('top');
+    }
+    if(col != 1){
+      col_validator.push('right');
+    }
+    if(col != 4){
+      col_validator.push('left');
+    }
+
+    var looper = 0;
+
+    row_validator.forEach(function(dataRow){
+      col_validator.forEach(function(dataCol){
+        if(check_block_recursive(row, col, dataRow, dataCol) == 1){
+          looper = 1;
+        }
+      });
+    });
+
+    return looper;
+  }
+
   //Check reult after user tap the pad
   function check_result(row, col){
     if(check_row(row) == 1){
@@ -81,6 +150,8 @@ $(document).ready(function(){
     }else if(check_cross_1() == 1){
       finish();
     }else if(check_cross_2() == 1){
+      finish();
+    }else if(check_block(row, col) == 1){
       finish();
     }
   }
@@ -109,9 +180,10 @@ $(document).ready(function(){
     pad_id = "#" + this.id;
     var row = parseInt(pad_id.slice(5, 6));
     var col = parseInt(pad_id.slice(7));
-    val[row][col] = (pad_value(step) == "X" ? 1 : 9);
+    val[row][col] = (pad_value(step) == "X" ? 10 : 100);
     $(pad_id).html(pad_value(step));
     $(pad_id).addClass("disabled");
+    // check_block(row, col);
     check_result(row, col);
     step++;
   });
